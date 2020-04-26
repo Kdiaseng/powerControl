6ddd9ccb5003ddd861073813380286f94b772647
kaleb.silva <kaleb.silva@itbam.org.br>
2020-04-25 22:53:44 -0400

    add datapicker in register screen

--- app/src/main/java/com/aplicativo/powercontrol/RegisterCountPowerFragment.kt
+++ app/src/main/java/com/aplicativo/powercontrol/RegisterCountPowerFragment.kt
@@ -1,15 +1,23 @@
 package com.aplicativo.powercontrol
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.fragment_register_count_power.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class RegisterCountPowerFragment : Fragment() {
+
+    val calendar = Calendar.getInstance()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +30,45 @@ class RegisterCountPowerFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+
+        TxtLayout_date_init.setEndIconOnClickListener {
+            DatePickerDialog(
+                this.requireContext(),
+                DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                    updateData(year, month, dayOfMonth, textInput_date_init)
+                },
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
+        TxtLayout_date_end.setEndIconOnClickListener {
+            DatePickerDialog(
+                this.requireContext(),
+                DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                    updateData(year, month, dayOfMonth, textInput_date_end)
+
+                },
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
+
     }
+
+    private fun updateData(year: Int, month: Int, dayOfMonth: Int, texField: TextInputEditText) {
+        calendar.set(Calendar.YEAR, year)
+        calendar.set(Calendar.MONTH, month)
+        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+        val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR"))
+        simpleDateFormat.format(calendar.time)
+        texField.setText(simpleDateFormat.format(calendar.time))
+    }
+
 
 }
